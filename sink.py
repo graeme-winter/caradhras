2871f7dd79da2094e76ee205c841e24a430349ce
Graeme Winter <graeme.winter@gmail.com>
2024-07-03 16:04:54 +0100

    Hostname on commandline

--- sink.py
+++ sink.py
@@ -1,12 +1,12 @@
 import os
 import zmq
+import sys
 
-host = "i24-ppu01.diamond.ac.uk"
 port = 4242
 
 context = zmq.Context()
 socket = context.socket(zmq.PULL)
-socket.connect(f"tcp://{host}:{port}")
+socket.connect(f"tcp://{sys.argv[1]}:{port}")
 
 while 1:
     filename, content = socket.recv_multipart()
